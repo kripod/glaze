af11dd6466fbbaaa4ec1bd0c6bf24690dba31880
Kristóf Poduszló <kripod@protonmail.com>
2020-03-25 19:49:35 +0100

    improvement(example): simplify

--- packages/gatsby-plugin-glaze-example/gatsby-config.js
+++ packages/gatsby-plugin-glaze-example/gatsby-config.js
@@ -1,26 +1,7 @@
-const siteMetadata = {
-  name: 'glaze',
-  description: 'CSS-in-JS framework for building approachable design systems.',
-};
-
 module.exports = {
-  siteMetadata,
   plugins: [
     'gatsby-plugin-glaze',
     'gatsby-plugin-treat',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        ...siteMetadata,
-        display: 'minimal-ui',
-        // TODO: theme_color: '#663399',
-        // TODO: background_color: 'white',
-        // TODO: icon: 'src/assets/favicon.png',
-        lang: 'en-US',
-        start_url: '/',
-      },
-    },
-    'gatsby-plugin-react-helmet',
     'gatsby-plugin-typescript',
   ],
 };
--- packages/gatsby-plugin-glaze-example/src/pages/index.tsx
+++ packages/gatsby-plugin-glaze-example/src/pages/index.tsx
@@ -1,16 +1,12 @@
 import { useStyling } from 'glaze';
 import React from 'react';
 
-import { Layout } from '../components/Layout';
-
 export default function IndexPage(): JSX.Element {
   const sx = useStyling();
 
   return (
-    <Layout>
-      <div {...sx({ px: 4, py: 2, bg: 'papayawhip', color: 'rebeccapurple' })}>
-        Hello, world!
-      </div>
-    </Layout>
+    <div {...sx({ px: 4, py: 2, bg: 'papayawhip', color: 'rebeccapurple' })}>
+      Hello, world!
+    </div>
   );
 }
