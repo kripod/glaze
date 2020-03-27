89c6c32381f21313bbf754e98be092572d7bf519
Kristóf Poduszló <kripod@protonmail.com>
2020-03-27 02:27:01 +0100

    fix(example): plugin build for Next.js

--- packages/example-nextjs/next.config.js
+++ packages/example-nextjs/next.config.js
@@ -1,18 +1,11 @@
 // @ts-nocheck
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const withTM = require('next-transpile-modules')(['glaze']);
 const TreatPlugin = require('treat/webpack-plugin');
 
 module.exports = withTM({
   webpack: (config, { isServer }) => {
-    config.plugins.push(
-      new TreatPlugin({
-        outputLoaders: [MiniCssExtractPlugin.loader],
-        outputCSS: !isServer,
-      }),
-      new MiniCssExtractPlugin(),
-    );
+    config.plugins.push(new TreatPlugin({ outputCSS: !isServer }));
     return config;
   },
 });
