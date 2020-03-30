38d79f5ec72215df3e5d1499d6faf2a2bb247367
Kristóf Poduszló <kripod@protonmail.com>
2020-03-30 19:13:33 +0200

    docs(example): improve periodic unmounting

--- packages/example-gatsby/src/pages/index.tsx
+++ packages/example-gatsby/src/pages/index.tsx
@@ -7,17 +7,18 @@ import { Text } from '../components/Text';
 export default function IndexPage(): JSX.Element {
   const sx = useStyling();
 
-  const [isTextMounted, setTextMounted] = useState(true);
-  useInterval(() => {
-    setTextMounted((value) => !value);
-  }, 3000);
+  const [isFirstTextMounted, setFirstTextMounted] = useState(true);
+  const [isSecondTextMounted, setSecondTextMounted] = useState(true);
+  useInterval(() => setFirstTextMounted((value) => !value), 3000);
+  useInterval(() => setSecondTextMounted((value) => !value), 5000);
 
   return (
     <>
       <h1 className={sx({ px: 4, color: 'white', bg: 'red' })}>
         Hello, world!
       </h1>
-      {isTextMounted && <Text>Lorem ipsum dolor sit amet</Text>}
+      {isFirstTextMounted && <Text>1: Lorem ipsum dolor sit amet</Text>}
+      {isSecondTextMounted && <Text>2: Lorem ipsum dolor sit amet</Text>}
     </>
   );
 }
