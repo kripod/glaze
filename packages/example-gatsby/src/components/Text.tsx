f5ff7acf717d304e0e30bb509d15b8f91d60d879
Kristóf Poduszló <kripod@protonmail.com>
2020-03-30 13:11:47 +0200

    docs(example): add dynamic style unmounting

---
+++ packages/example-gatsby/src/components/Text.tsx
@@ -0,0 +1,17 @@
+import { useStyling } from 'glaze';
+import React from 'react';
+
+export interface TextProps extends React.HTMLAttributes<HTMLParagraphElement> {
+  children: React.ReactNode;
+}
+
+export function Text({ className = '', ...restProps }: TextProps): JSX.Element {
+  const sx = useStyling();
+
+  return (
+    <p
+      className={`${className} ${sx({ color: 'white', bg: 'black' })}`}
+      {...restProps}
+    />
+  );
+}
--- packages/example-gatsby/src/pages/index.tsx
+++ packages/example-gatsby/src/pages/index.tsx
@@ -1,17 +1,23 @@
 import { useStyling } from 'glaze';
-import React from 'react';
+import React, { useState } from 'react';
+import { useInterval } from 'web-api-hooks';
+
+import { Text } from '../components/Text';
 
 export default function IndexPage(): JSX.Element {
   const sx = useStyling();
+
+  const [isTextMounted, setTextMounted] = useState(true);
+  useInterval(() => {
+    setTextMounted((value) => !value);
+  }, 3000);
 
   return (
     <>
       <h1 className={sx({ px: 4, color: 'white', bg: 'red' })}>
         Hello, world!
       </h1>
-      <p className={sx({ color: 'white', bg: 'black' })}>
-        Lorem ipsum dolor sit amet
-      </p>
+      {isTextMounted && <Text>Lorem ipsum dolor sit amet</Text>}
     </>
   );
 }
