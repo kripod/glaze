ad72f13dffc31af28309012ce971e4f66625b629
Kristóf Poduszló <kripod@protonmail.com>
2020-03-31 22:47:01 +0200

    docs(example): improve Next.js SSR

--- packages/example-nextjs/next.config.js
+++ packages/example-nextjs/next.config.js
@@ -1,11 +1,23 @@
 // @ts-nocheck
 
+const {
+  getClientStyleLoader,
+} = require('next/dist/build/webpack/config/blocks/css/loaders/client');
 const withTM = require('next-transpile-modules')(['glaze']);
 const TreatPlugin = require('treat/webpack-plugin');
 
 module.exports = withTM({
-  webpack: (config, { isServer }) => {
-    config.plugins.push(new TreatPlugin({ outputCSS: !isServer }));
+  webpack: (config, { dev, isServer, config: { assetPrefix } }) => {
+    config.plugins.push(
+      new TreatPlugin({
+        outputCSS: !isServer,
+        outputLoaders: [
+          !isServer
+            ? getClientStyleLoader({ isDevelopment: dev, assetPrefix })
+            : 'style-loader',
+        ],
+      }),
+    );
     return config;
   },
 });
