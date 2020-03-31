45009676c3b4046e8c00fb613eac6a1f6fad0619
Kristóf Poduszló <kripod@protonmail.com>
2020-03-31 23:05:07 +0200

    improvement(example): remove unnecessary server-side CSS loader

--- packages/example-nextjs/next.config.js
+++ packages/example-nextjs/next.config.js
@@ -12,9 +12,11 @@ module.exports = withTM({
       new TreatPlugin({
         outputCSS: !isServer,
         outputLoaders: [
+          // Extract static CSS in production
+          // Logic adopted from https://github.com/zeit/next.js/blob/ee0081356d7ea166dfed4765f134730c11ecaecf/packages/next/build/webpack/config/blocks/css/loaders/global.ts#L13-L22
           !isServer
             ? getClientStyleLoader({ isDevelopment: dev, assetPrefix })
-            : 'style-loader',
+            : '',
         ],
       }),
     );
