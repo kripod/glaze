14ebde6800be207b28d7e7712bb6761b92c4ebc2
Kristóf Poduszló <kripod@protonmail.com>
2020-04-01 14:21:27 +0200

    improvement: abstract away style mounting logic

--- packages/glaze/src/GlazeContext.tsx
+++ packages/glaze/src/GlazeContext.tsx
@@ -1,3 +1,4 @@
+import hash from '@emotion/hash';
 import React, { useContext } from 'react';
 import { TreatProvider } from 'react-treat';
 
@@ -5,7 +6,8 @@ import { RuntimeTheme } from './theme';
 
 export const GlazeContext = React.createContext<{
   theme: RuntimeTheme;
-  instancesByClassName: Map<string, number>;
+  mountStyle: (identName: string, cssText: () => string) => string;
+  unmountStyle: (usageCount: number, className: string) => void;
 }>(undefined as never);
 
 export interface ThemeProviderProps {
@@ -17,12 +19,54 @@ export function ThemeProvider({
   theme,
   children,
 }: ThemeProviderProps): JSX.Element {
+  const usageCountsByClassName = new Map<string, number>();
+
   return (
     <TreatProvider
       // Show a clear error message during runtime, even if `theme` is nullish
       theme={theme?.ref}
     >
-      <GlazeContext.Provider value={{ theme, instancesByClassName: new Map() }}>
+      <GlazeContext.Provider
+        value={{
+          theme,
+
+          mountStyle(identName, cssText): string {
+            // TODO: Use same hashing algorithm during static CSS generation
+            const className =
+              process.env.NODE_ENV !== 'production'
+                ? `DYNAMIC_${identName}`
+                : `d_${hash(identName)}`;
+
+            const usageCount = usageCountsByClassName.get(className) || 0;
+            if (!usageCount) {
+              const element = document.createElement('style');
+              element.id = className;
+              element.textContent = `.${className}{${cssText()}}`;
+              document.head.appendChild(element);
+            }
+            usageCountsByClassName.set(className, usageCount + 1);
+
+            return className;
+          },
+
+          unmountStyle(usageCount, className): void {
+            const remainingInstances =
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              usageCountsByClassName.get(className)! - usageCount;
+
+            if (remainingInstances) {
+              usageCountsByClassName.set(className, remainingInstances);
+            } else {
+              usageCountsByClassName.delete(className);
+
+              // Detach unused dynamic style from the DOM
+              // TODO: Use `ChildNode.remove()` when dropping IE 11 support
+              const element = document.getElementById(className);
+              if (element) document.head.removeChild(element);
+            }
+          },
+        }}
+      >
         {children}
       </GlazeContext.Provider>
     </TreatProvider>
--- packages/glaze/src/useStyling.ts
+++ packages/glaze/src/useStyling.ts
@@ -19,29 +19,13 @@ export type ThemedStyle = Style & {
 
 export function useStyling(): (themedStyle: ThemedStyle) => string {
   const staticClassNames = useStyles(styleRefs);
-  const { theme, instancesByClassName } = useContext(GlazeContext);
-  const ownedInstancesByClassName = useRef(new Map<string, number>()).current;
+  const { theme, mountStyle, unmountStyle } = useContext(GlazeContext);
+  const ownUsageCountByClassName = useRef(new Map<string, number>()).current;
 
   // Remove dynamic styles which are not used anymore when unmounting
   useEffect(
     () => (): void => {
-      ownedInstancesByClassName.forEach((usageCount, className) => {
-        const remainingInstances =
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          instancesByClassName.get(className)! - usageCount;
-
-        if (remainingInstances) {
-          instancesByClassName.set(className, remainingInstances);
-        } else {
-          // Detach unused dynamic style from the DOM
-          // TODO: Use `ChildNode.remove()` when dropping IE 11 support
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          const element = document.getElementById(className)!;
-          document.head.removeChild(element);
-
-          instancesByClassName.delete(className);
-        }
-      });
+      ownUsageCountByClassName.forEach(unmountStyle);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
@@ -59,32 +43,25 @@ export function useStyling(): (themedStyle: ThemedStyle) => string {
       (theme.shorthands[shorthand] || [shorthand]).forEach((key) => {
         // TODO: Support selectors and media queries
         if (typeof value !== 'object') {
-          let appendedClassName = staticClassNames[`${key}-${value}`];
+          const identName = `${key}-${value}`;
+          let appendedClassName = staticClassNames[identName];
 
           // Attach a class dynamically if needed
           // TODO: Improve support for SSR
           if (!appendedClassName && typeof window !== 'undefined') {
             // TODO: Use same hashing algorithm during static CSS generation
-            appendedClassName =
-              process.env.NODE_ENV !== 'production'
-                ? `DYNAMIC_${key}-${value}`
-                : `d_${hash(`${key}-${value}`)}`;
-            let usageCount = instancesByClassName.get(appendedClassName);
-            if (!usageCount) {
-              usageCount = 0;
-              // Convert CSS property to kebab-case and normalize numeric value
-              const style = `${key.replace(/[A-Z]/g, kebabCaseReplacer)}:${
-                typeof value !== 'number' ? value : `${value}px`
-              }`;
-              const element = document.createElement('style');
-              element.id = appendedClassName;
-              element.textContent = `.${appendedClassName}{${style}}`;
-              document.head.appendChild(element);
-            }
-            instancesByClassName.set(appendedClassName, usageCount + 1);
-            ownedInstancesByClassName.set(
+            appendedClassName = mountStyle(
+              identName,
+              () =>
+                // Convert CSS property to kebab-case and normalize numeric value
+                `${key.replace(/[A-Z]/g, kebabCaseReplacer)}:${
+                  typeof value !== 'number' ? value : `${value}px`
+                }`,
+            );
+
+            ownUsageCountByClassName.set(
               appendedClassName,
-              (ownedInstancesByClassName.get(appendedClassName) || 0) + 1,
+              (ownUsageCountByClassName.get(appendedClassName) || 0) + 1,
             );
           }
 
