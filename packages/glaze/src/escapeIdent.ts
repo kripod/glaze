9f728ed889f51df1dd6352e8e0cd0f67995e8df2
Kristóf Poduszló <kripod@protonmail.com>
2020-04-08 01:35:32 +0200

    chore(escape): add notes

--- packages/glaze/src/escapeIdent.ts
+++ packages/glaze/src/escapeIdent.ts
@@ -1,4 +1,6 @@
 export default function escapeIdent(ident: string): string {
   // TODO: Replace with `CSS.escape` when widely supported by browsers
+  // Source: https://www.w3.org/TR/cssom-1/#serialize-an-identifier
+  // Control characters and non-letter first symbols are not supported
   return ident.replace(/[!"#$%&'()*+,./:;<=>?@[\\\]^`{|}~]/g, '\\$&');
 }
