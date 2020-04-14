ef67a2e77cc4045e778cb46fe1e59f39351a017c
Kristóf Poduszló <kripod@protonmail.com>
2020-04-14 17:26:58 +0200

    refactor: modularize package install snippet

---
+++ packages/website/src/theme/PackageInstall.tsx
@@ -0,0 +1,35 @@
+import CodeBlock from '@theme/CodeBlock';
+import TabItem from '@theme/TabItem';
+import Tabs from '@theme/Tabs';
+import React from 'react';
+
+export interface PackageInstallProps {
+  dependencies?: string[];
+}
+
+export default function PackageInstall({
+  dependencies,
+}: PackageInstallProps): JSX.Element {
+  return (
+    <Tabs
+      groupId="package-managers"
+      defaultValue="npm"
+      values={[
+        { label: 'npm', value: 'npm' },
+        { label: 'Yarn', value: 'yarn' },
+      ]}
+    >
+      <TabItem value="npm">
+        <CodeBlock className="shell">
+          {dependencies && `npm install ${dependencies.join(' ')}`}
+        </CodeBlock>
+      </TabItem>
+
+      <TabItem value="yarn">
+        <CodeBlock className="shell">
+          {dependencies && `yarn add ${dependencies.join(' ')}`}
+        </CodeBlock>
+      </TabItem>
+    </Tabs>
+  );
+}
