60910c53a20191ce5e879f355773bd92123b82a4
Kristóf Poduszló <kripod@protonmail.com>
2020-04-15 15:18:10 +0200

    docs: add section about contributing

--- packages/website/sidebars.js
+++ packages/website/sidebars.js
@@ -2,5 +2,10 @@ module.exports = {
   docs: {
     Introduction: ['getting-started', 'autocompletion', 'how-it-works'],
     Features: ['theming', 'server-side-rendering'],
+    Contributing: [
+      'policies-and-procedures',
+      'code-of-conduct',
+      'architectural-overview',
+    ],
   },
 };
