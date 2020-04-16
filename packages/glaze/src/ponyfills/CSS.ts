de97e10e3c4ba6c71c032dafad18a58b634c499b
Kristóf Poduszló <kripod@protonmail.com>
2020-04-16 13:09:28 +0200

    perf: optimize CSS ident escaper RegExp

--- packages/glaze/src/ponyfills/CSS.ts
+++ packages/glaze/src/ponyfills/CSS.ts
@@ -5,5 +5,21 @@
 // eslint-disable-next-line import/prefer-default-export
 export function escape(ident: string): string {
   // Source: https://www.w3.org/TR/cssom-1/#serialize-an-identifier
-  return ident.replace(/[!"#$%&'()*+,./:;<=>?@[\\\]^`{|}~]/g, '\\$&');
+  return ident.replace(
+    /**
+     * // Generated with https://github.com/mathiasbynens/regenerate
+     * new RegExp(
+     *   regenerate()
+     *     .addRange(0x20, 0x7e)
+     *     .remove('-', '_')
+     *     .removeRange('0', '9')
+     *     .removeRange('A', 'Z')
+     *     .removeRange('a', 'z')
+     *     .toString(),
+     *   'g',
+     * )
+     */
+    /[ -,./:-@[-^`{-~]/g,
+    '\\$&',
+  );
 }
