892ea21ab22c8a273f7b6d5c05c6fba0e8cf5d4f
Kristóf Poduszló <kripod@protonmail.com>
2020-04-18 02:10:26 +0200

    chore(babel-plugin): fix scripts and Babel config

--- packages/babel-plugin-glaze/babel.config.js
+++ packages/babel-plugin-glaze/babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  presets: [
+    ['@babel/preset-env', { targets: { node: '10' } }],
+    '@babel/preset-typescript',
+  ],
 };
