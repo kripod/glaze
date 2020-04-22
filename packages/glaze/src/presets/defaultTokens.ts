898788265eda82ea4172bba859a898f5f6bab92c
Kristóf Poduszló <kripod@protonmail.com>
2020-04-22 03:42:52 +0200

    chore: suppress Gatsby's linter warning

--- packages/glaze/src/presets/defaultTokens.ts
+++ packages/glaze/src/presets/defaultTokens.ts
@@ -215,5 +215,5 @@ export const defaultTokens = {
   },
 } as const;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
 const assertTypeCompatibility: StaticTheme = defaultTokens;
