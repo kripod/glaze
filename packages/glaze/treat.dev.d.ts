6d26e456dc5bf2f3e99148bdec466e613063dd8f
Kristóf Poduszló <kripod@protonmail.com>
2020-04-28 19:01:04 +0200

    chore: remove `treat.dev.d.ts`

--- packages/glaze/treat.dev.d.ts
+++
@@ -1,7 +0,0 @@
-import { defaultTokens } from './src';
-
-declare module 'treat/theme' {
-  type Tokens = typeof defaultTokens;
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface Theme extends Tokens {}
-}
